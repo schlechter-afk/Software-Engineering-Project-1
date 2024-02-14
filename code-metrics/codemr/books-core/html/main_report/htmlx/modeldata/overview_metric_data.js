0e77692233fa18c60663ee37f34cf159cff25678
FlightVin <vineeth.bhat@gmail.com>
2024-02-15 01:41:51 +0530

    Add codemr metrics

---
+++ code-metrics/codemr/books-core/html/main_report/htmlx/modeldata/overview_metric_data.js
@@ -0,0 +1,1 @@
+var CodeMR_ProjectMetrics = {"Project Name":"se-project-1--_14", "Number of Highly Problematic Classes":0, "Number of Entities":71, "Number of Problematic Classes":0, "Class Lines of Code":2523, "Number of External Packages":0, "Number of Packages":20, "Number of External Entities":0};var EQ_COLOR_OF_LEVELS = ["#1F77B4","#007F24","#62BF18","#FFC800","#FF5B13","#E50000"];
